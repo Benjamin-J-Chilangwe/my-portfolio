f8fb5fe921754a52c8fbe27b5bf47e0cdc5a359a
Benjamin-J-Chilangwe <benchilangwe9949@gmail.com>
2025-07-27 06:25:59 +0200

    Update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,21 +5,21 @@ const projects = [
     title: "Findit App",
     description: "A lost-and-found mobile app using Flutter and Firebase.",
     image:
-      "https://ypvhmhcgrbyqzlpftlzt.supabase.co/storage/v1/object/sign/assets/FindIt.png?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9mN2FiZGE5Zi0xMzIzLTQ0OTQtOWM0Ni1kYmRhNTEwZjAzYWQiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJhc3NldHMvRmluZEl0LnBuZyIsImlhdCI6MTc1MjMzMDg1NSwiZXhwIjoxNzgzODY2ODU1fQ.7VmOqqcM6Yh2jtllke-2iusiCGNM97w8I1xRzx1D0Os",
+      "https://ypvhmhcgrbyqzlpftlzt.supabase.co/storage/v1/object/sign/assets/FindIt.png?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV8xOTFmY2YyNS0yMmY5LTQ4YWItOTczZS0zYzEwMGZlZWMwMjkiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJhc3NldHMvRmluZEl0LnBuZyIsImlhdCI6MTc1MzU4OTUxOSwiZXhwIjoxNzg1MTI1NTE5fQ.oK4f_6UBk5ckPhX0mDDQ3S7xXbg4RBD7hIaxXjcsjFs",
     link: "https://github.com/bsc-inf-08-20/findit.git",
   },
   {
     title: "Educonnect WebApp",
     description: "An online learning smart classroom platform.",
     image:
-      "https://ypvhmhcgrbyqzlpftlzt.supabase.co/storage/v1/object/sign/assets/edu.png?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9mN2FiZGE5Zi0xMzIzLTQ0OTQtOWM0Ni1kYmRhNTEwZjAzYWQiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJhc3NldHMvZWR1LnBuZyIsImlhdCI6MTc1MjMzMDg5NiwiZXhwIjoxNzgzODY2ODk2fQ.1rO031gHja7Qlhd2z8BT_h1Xnvwir-vRN-IGNHc8uWA",
+      "https://ypvhmhcgrbyqzlpftlzt.supabase.co/storage/v1/object/sign/assets/edu.png?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV8xOTFmY2YyNS0yMmY5LTQ4YWItOTczZS0zYzEwMGZlZWMwMjkiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJhc3NldHMvZWR1LnBuZyIsImlhdCI6MTc1MzU4OTYwOCwiZXhwIjoxNzg1MTI1NjA4fQ.PRuO9dcl7yDji9pR3OZaLth9rtutRkZ0uKEi6ue3Mfs",
     link: "https://www.educonnecti.site/",
   },
   {
     title: "Portfolio-1",
     description: "A little description and overview of myself and my skills.",
     image:
-      "https://ypvhmhcgrbyqzlpftlzt.supabase.co/storage/v1/object/sign/assets/Portfolio.png?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9mN2FiZGE5Zi0xMzIzLTQ0OTQtOWM0Ni1kYmRhNTEwZjAzYWQiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJhc3NldHMvUG9ydGZvbGlvLnBuZyIsImlhdCI6MTc1MjMzMDgwNiwiZXhwIjoxNzgzODY2ODA2fQ.DfkCj_kNCyFz38AlTVPbK83exvvT2Wz0RSKsTgvThN0",
+      "https://ypvhmhcgrbyqzlpftlzt.supabase.co/storage/v1/object/sign/assets/Portfolio.png?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV8xOTFmY2YyNS0yMmY5LTQ4YWItOTczZS0zYzEwMGZlZWMwMjkiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJhc3NldHMvUG9ydGZvbGlvLnBuZyIsImlhdCI6MTc1MzU5MDE4NSwiZXhwIjoxNzg1MTI2MTg1fQ.rDZ6bPJ_l3ZbG3ODuv73VbT-x1Wy2soIsFmPR9caLlQ",
     link: "https://magenta-haupia-42ba2c.netlify.app/",
   },
 ];
